3541c538e83154364e71a5bd7ffcfa2c8551c136
digiponic-renal <renaldy@digiponic.co.id>
2019-02-11 09:39:55 +0700

    bottom sheet collapse

--- app/src/main/java/com/npe/youji/MainActivity.java
+++ app/src/main/java/com/npe/youji/MainActivity.java
@@ -2,26 +2,37 @@ package com.npe.youji;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.npe.youji.fragment.LoginFragment;
 import com.npe.youji.fragment.OrderFragment;
 import com.npe.youji.fragment.shop.ShopFragment;
 
+import at.markushi.ui.CircleButton;
+
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigation;
+    BottomSheetBehavior botomSheet;
+    RelativeLayout layoutBottomSheet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //insialisasi
         bottomNavigation = findViewById(R.id.bottom_navigation);
+        layoutBottomSheet = findViewById(R.id.bottom_sheet);
+        botomSheet = BottomSheetBehavior.from(layoutBottomSheet);
 
+        //bottom navigation
         bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -41,6 +52,35 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                 }
                 return false;
+            }
+        });
+
+        //bottom sheet
+        botomSheet.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+                switch (i){
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED : {
+
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED : {
+
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING :
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING :
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+
             }
         });
 
