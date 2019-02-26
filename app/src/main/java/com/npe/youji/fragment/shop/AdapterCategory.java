db2f6c4d49b84d4f67f311739e1bdb4790e6918a
digiponic-renal <renaldy@digiponic.co.id>
2019-02-26 23:04:50 +0700

    local -product by category not done

--- app/src/main/java/com/npe/youji/fragment/shop/AdapterCategory.java
+++ app/src/main/java/com/npe/youji/fragment/shop/AdapterCategory.java
@@ -1,8 +1,11 @@
 package com.npe.youji.fragment.shop;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.SQLException;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,17 +13,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
 import com.npe.youji.R;
+import com.npe.youji.activity.DetailShop;
+import com.npe.youji.model.dbsqlite.ShopOperations;
+import com.npe.youji.model.shop.JoinModel;
 import com.npe.youji.model.shop.menu.DataCategory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AdapterCategory extends RecyclerView.Adapter<AdapterCategory.ViewHolder> {
     private Context context;
     private List<DataCategory> items;
+    private Gson gson;
+    ShopOperations shopOperations;
+    ArrayList<JoinModel> dataJoin;
 
     public AdapterCategory(Context context, List<DataCategory> items) {
         this.context = context;
+        shopOperations = new ShopOperations(context);
         this.items = items;
     }
 
@@ -32,14 +44,44 @@ public class AdapterCategory extends RecyclerView.Adapter<AdapterCategory.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
         DataCategory data = items.get(i);
+        //join data
+        getDataJoin();
 
         Glide.with(context)
                 .load(data.getImage())
                 .into(viewHolder.image);
 
         viewHolder.nama.setText(data.getName());
+
+//        viewHolder.image.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                toDetail(i);
+//            }
+//        });
+    }
+
+
+    private void getDataJoin() {
+        try{
+            shopOperations.openDb();
+            dataJoin = shopOperations.joinData();
+            shopOperations.closeDb();
+        }catch (SQLException e){
+            Log.i("ErrorGetDataJoin", e.getMessage());
+        }
+    }
+
+
+    private void toDetail(int position) {
+        gson = new Gson();
+        String json = gson.toJson(dataJoin.get(position));
+        Intent intent = new Intent(context, DetailShop.class);
+        intent.putExtra("DATA", json);
+        context.startActivity(intent);
     }
 
     @Override
