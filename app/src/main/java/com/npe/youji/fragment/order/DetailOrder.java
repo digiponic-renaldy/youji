4852ee7d4d5cdb6a71fb91b0dff755490101e106
digiponic-renal <renaldy@digiponic.co.id>
2019-02-28 13:19:31 +0700

    local - add detail order

---
+++ app/src/main/java/com/npe/youji/fragment/order/DetailOrder.java
@@ -0,0 +1,15 @@
+package com.npe.youji.fragment.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.npe.youji.R;
+
+public class DetailOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_order);
+    }
+}
